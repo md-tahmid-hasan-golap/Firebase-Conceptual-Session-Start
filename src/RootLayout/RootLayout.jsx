7082c52fe2c25e37a3dac24730aed601d88cc295
md-tahmid-hasan-golap <golapraj47@gmail.com>
2025-06-05 22:46:38 +0600

    resolve

--- src/RootLayout/RootLayout.jsx
+++ src/RootLayout/RootLayout.jsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { createContext } from "react";
 import { Outlet } from "react-router";
 import Navbar from "../Components/Navbar/Navbar";
+export const creatconText = createContext();
 
 const RootLayout = () => {
+  const a = 10;
   return (
     <div>
       <Navbar></Navbar>
-      <Outlet></Outlet>
+      <creatconText.Provider value={a}>
+        <Outlet></Outlet>
+      </creatconText.Provider>
     </div>
   );
 };
